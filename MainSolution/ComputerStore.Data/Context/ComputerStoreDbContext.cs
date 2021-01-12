c7edc6d0be096da440d2914660bf0719f711489a
Izaak <izaak.bajada@gmail.com>
2021-01-12 14:43:50 +0100

    Updating nuget packages across all projects

---
+++ MainSolution/ComputerStore.Data/Context/ComputerStoreDbContext.cs
@@ -0,0 +1,28 @@
+﻿using ComputerStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerStore.Data.Context
+{
+    public class ComputerStoreDbContext : DbContext
+    {
+        public ComputerStoreDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
---
+++ MainSolution/ComputerStore.Domain/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ComputerStore.Domain.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public virtual Category Category { get; set; }
+    }
+}
